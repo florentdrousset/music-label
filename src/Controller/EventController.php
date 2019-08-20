b6187100ac458a94fde77662ca6a2566fbd99838
Florent Drousset <florent.drousset@gmail.com>
2019-08-20 14:54:20 +0200

    systeme d'upload d'images ajouté

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -9,6 +9,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Product;
+use App\Form\ProductType;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 /**
  * @Route("/event")
@@ -24,6 +28,14 @@ class EventController extends AbstractController
             'events' => $eventRepository->findAll(),
         ]);
     }
+    /**
+     * @Route("/", name="event_public", methods={"GET"})
+     */
+    public function showEvents(EventRepository $eventRepository) {
+        return $this->render('event/publicIndex.html.twig', [
+            'events' => $eventRepository->findAll()
+            ]);
+    }
 
     /**
      * @Route("/new", name="event_new", methods={"GET","POST"})
@@ -35,6 +47,31 @@ class EventController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            /** @var UploadedFile $img */
+            $img = $form['img']->getData();
+
+            // this condition is needed because the 'brochure' field is not required
+            // so the PDF file must be processed only when a file is uploaded
+            if ($img) {
+                $originalFilename = pathinfo($img->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$img->guessExtension();
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $img->move(
+                        $this->getParameter('img_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                $event->setImg($newFilename);
+            }
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($event);
             $entityManager->flush();
--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -9,6 +9,9 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use App\Entity\Artist;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
 
 class EventType extends AbstractType
 {
@@ -26,7 +29,30 @@ class EventType extends AbstractType
                 'class' => Artist::class,
                 'choice_label' => 'name'
             ])
-            ->add('img')
+            //->add('img')
+            ->add('img', FileType::class, [
+                'label' => 'Image',
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // everytime you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '4000k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid PNG image',
+                    ])
+                ],
+            ])
         ;
     }
 
